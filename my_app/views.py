0bd1a2c1563e0940f5de99204ab0f028224b6378
gabtongol <geybtongol@gmail.com>
2024-09-11 18:01:52 -0400

    render home view

--- my_app/views.py
+++ my_app/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>CommUnity Board</h1>')
+    return render(request, 'home.html')
+
 
 def about(request):
-    return HttpResponse('<h1>About CommUnity Board</h1>')
+    return render(request, 'about.html')
